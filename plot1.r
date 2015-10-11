fe99b5746059253f9a5771e90e240469f305269d
Melechever <melechever@gmail.com>
2015-10-11 18:56:58 -0430

    Plot 1 Corregido

--- plot1.r
+++ plot1.r
@@ -4,7 +4,6 @@ library(data.table)
 library(datasets)
 library(dplyr)
 data<-fread("household_power_consumption.txt")
-data$Date<-as.Date(data$Date,format = "%d/%m/%Y")
 mydata<-filter(data,Date=="1/2/2007" | Date == "2/2/2007")
 mydata$Global_active_power<-as.numeric(mydata$Global_active_power)
 
