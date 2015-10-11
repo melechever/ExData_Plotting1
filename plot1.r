d35df405359c86a0471b7c667b429e717bb52de0
Melechever <melechever@gmail.com>
2015-10-11 18:59:31 -0430

    Plot 1 Color

--- plot1.r
+++ plot1.r
@@ -8,5 +8,5 @@ mydata<-filter(data,Date=="1/2/2007" | Date == "2/2/2007")
 mydata$Global_active_power<-as.numeric(mydata$Global_active_power)
 
 png(filename='plot1.png', width=480, height=480, units='px')
-hist(mydata$Global_active_power, main="Global Active Power",xlab="Global Active Power (kilowatts)")
+hist(mydata$Global_active_power, main="Global Active Power",col="red",xlab="Global Active Power (kilowatts)")
 dev.off()
